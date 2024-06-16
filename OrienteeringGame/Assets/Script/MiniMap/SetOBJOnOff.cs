4431c7907ae305158c8f951e791b1f8ecda52527
Cristina-Croitoru <ccroitoru623@gmail.com>
2024-06-16 14:34:07 +0300

    yep

---
+++ OrienteeringGame/Assets/Script/MiniMap/SetOBJOnOff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SetOBJOnOff : MonoBehaviour
+{
+    [Header("On/Off OBJ")]
+    public bool onoff;
+
+    [Header("OBJ to On/OFF")]
+    public GameObject UIPLayerOBJ;
+
+    private void Start()
+    {
+        UIPLayerOBJ.SetActive(onoff);
+    }
+
+}
